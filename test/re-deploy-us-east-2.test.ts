5952b760de6c146c4fa399a7e76a46565d13c889
an-nguyen <an.nguyen@hp.com>
2024-08-22 12:43:09 -0700

    Initial commit

---
+++ test/re-deploy-us-east-2.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ReDeployUsEast2 from '../lib/re-deploy-us-east-2-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/re-deploy-us-east-2-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ReDeployUsEast2.ReDeployUsEast2Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
